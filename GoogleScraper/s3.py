ed4313475b3f35eab29cdead4a5e8d09d7ac0546
Daniel Matysiak <daniel.matysiak@l2inc.com>
2015-12-07 18:27:27 -0500

    Load ENV vars differently

--- GoogleScraper/s3.py
+++ GoogleScraper/s3.py
@@ -12,9 +12,7 @@ import tinys3
 ### 
 
 SCRAPER_TO_LOAD = 'scraper_to_load'
-AMAZON_WEB_SERVICES_ACCESS_KEY = self._env.get('AMAZON_WEB_SERVICES_ACCESS_KEY')
-AMAZON_WEB_SERVICES_SECRET_KEY = self._env.get('AMAZON_WEB_SERVICES_SECRET_KEY')
-RAVANA_S3_BUCKET = self._env.get('RAVANA_S3_BUCKET')
+
 
 class S3Table:
     
@@ -22,6 +20,11 @@ class S3Table:
         self._table_obj = table_obj
         self._scrape_id = scrape_id
         self._env = env
+        ##
+        self.ravana_s3_bucketamazon_web_services_access_key = self._env.get('AMAZON_WEB_SERVICES_ACCESS_KEY')
+        self.amazon_web_services_secret_key = self._env.get('AMAZON_WEB_SERVICES_SECRET_KEY')
+        self.ravana_s3_bucket = self._env.get('RAVANA_S3_BUCKET')
+
         ##
         self._tablename = table_obj.__tablename__
         self._table_file = '{0}-{1}.csv'.format(self._tablename, self._scrape_id)
@@ -36,14 +39,14 @@ class S3Table:
 
         
     def write_buffer_to_s3(self):
-        conn = tinys3.Connection(AMAZON_WEB_SERVICES_ACCESS_KEY,
-                                 AMAZON_WEB_SERVICES_SECRET_KEY)
+        conn = tinys3.Connection(self.amazon_web_services_access_key,
+                                 self.amazon_web_services_secret_key)
         content = BytesIO(self._buffer.getvalue().encode('utf-8'))
         conn.upload(self._manifest_file, content, SCRAPER_TO_LOAD)
 
         manifest_content = BytesIO(StringIO(json.dumps(
             {"entries": [{"url": "s3://{0}/{1}".format(
-                RAVANA_S3_BUCKET,
+                self.ravana_s3_bucket,
                 self._table_file)}]}).getvalue().encode('utf-8')))
         conn.upload(self._manifest_file, manifest_content, SCRAPER_TO_LOAD)
 
