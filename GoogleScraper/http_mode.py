34562401e29eaba0df2b1c6f9365c0c65164e952
Daniel Matysiak <daniel.matysiak@l2inc.com>
2016-03-22 17:15:08 -0400

    More tweaks :D

--- GoogleScraper/http_mode.py
+++ GoogleScraper/http_mode.py
@@ -178,7 +178,7 @@ class HttpScrape(SearchEngineScrape, threading.Timer):
             logger.critical('blekko doesnt support http mode.')
             self.startable = False
 
-        self.serp_log = {}
+        self.serp_log = serp_log
 
     def set_proxy(self):
         """Setup a socks connection for the socks module bound to this instance.
